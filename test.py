eb644c35afde3887868b41b0a9cb4b4abac3fed2
Fabio Petroni <fabio.petroni.1986@gmail.com>
2021-07-07 17:48:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 from utils.function_normalizer import FunctionNormalizer
 from utils.instructions_converter import InstructionsConverter
 from utils.capstone_disassembler import disassemble
